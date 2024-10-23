84158c13f97e86965f0b9101900b8548361ded5e
eivorobuchek <enkavorobuchek@gmail.com>
2024-10-24 00:42:19 +0300

    add grpc for auth_service

---
+++ auth_service/client/main.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	pb "auth_service/pkg/api/auth"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:8081",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	cli := pb.NewAuthServiceClient(conn)
+
+	// /RegisterUser
+	{
+		_, err := cli.Register(context.Background(), &pb.RegisterRequest{
+			Email:    "email",
+			Password: "password",
+		})
+		if err != nil {
+			log.Fatalf("RegisterUser error: %v", err)
+		} else {
+			log.Printf("user success register")
+		}
+	}
+
+	// /LoginUser
+	{
+		resp, err := cli.Login(context.Background(), &pb.LoginRequest{
+			Email:    "email",
+			Password: "password",
+		})
+		if err != nil {
+			log.Fatalf("LoginErr error: %v", err)
+		} else {
+			// для Marshal proto сообщений в JSON необходимо использовать пакет protojson
+			notes, err := protojson.Marshal(resp)
+			if err != nil {
+				log.Fatalf(" protojson.Marshal error: %v", err)
+			} else {
+				log.Printf("notes: %s", string(notes))
+			}
+		}
+	}
+}
